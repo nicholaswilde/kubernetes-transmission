d7c51a4de983d7608996cdb0fc19f12df46acbbe
Nicholas Wilde <ncwilde43@gmail.com>
2020-10-22 00:32:13 -0700

    Fixed configMap typo

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ kubectl apply -f manifests/namespace.yaml
 kubectl apply -f manifests/nfs-persistentVolume.yaml
 kubectl apply -f manifests/persistentVolumeClaim.yaml
 kubectl apply -f manifests/secret.yaml
-kubectl apply -f manifests/confgMap.yaml
+kubectl apply -f manifests/configMap.yaml
 kubectl apply -f manifests/service.yaml
 kubectl apply -f manifests/deployment.yaml
 kubectl apply -f manifests/ingress.yaml
