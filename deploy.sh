c3f51d4f4e6f9b02231f3dd6a9f2c9f3f3d64ea2
Nicholas Wilde <ncwilde43@gmail.com>
2020-10-22 00:25:12 -0700

    Some stuff

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-if ! command -v kubectl2 &> /dev/null; then
-    kubectl is not installed
+if ! command -v kubectl &> /dev/null; then
+    echo 'kubectl is not installed'
     exit 1
 fi
+
+kubectl apply -f manifests/namespace.yaml
+kubectl apply -f manifests/nfs-persistentVolume.yaml
+kubectl apply -f manifests/persistentVolumeClaim.yaml
+kubectl apply -f manifests/secret.yaml
+kubectl apply -f manifests/confgMap.yaml
+kubectl apply -f manifests/service.yaml
+kubectl apply -f manifests/deployment.yaml
+kubectl apply -f manifests/ingress.yaml
