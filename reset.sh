5424ce76c70b2da373128b8d027f9e89a93f0fd6
Nicholas Wilde <ncwilde43@gmail.com>
2020-10-22 00:35:18 -0700

    Added reset.sh

---
+++ reset.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if ! command -v kubectl &> /dev/null; then
+    echo 'kubectl is not installed'
+    exit 1
+fi
+
+kubectl delete all --all -n transmission
+kubectl delete ingress transmission -n transmission
+kubectl delete pvc transmission-pvc -n transmission
+kubectl delete pv pv-nfs
+kubectl delete secret transmission-creds -n transmission
+kubectl delete namespace transmission
