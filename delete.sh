b197b5cf359825e2b411d41705b2ad4aa6ed13e9
Nicholas Wilde <ncwilde43@gmail.com>
2020-10-26 13:11:20 -0700

    Updated nfs pv name and renamed some files

--- delete.sh
+++ delete.sh
@@ -8,6 +8,6 @@ fi
 kubectl delete all --all -n transmission
 kubectl delete ingress transmission -n transmission
 kubectl delete pvc transmission-pvc -n transmission
-kubectl delete pv pv-nfs
+kubectl delete pv transmission-pv-nfs
 kubectl delete secret transmission-creds -n transmission
 kubectl delete namespace transmission
